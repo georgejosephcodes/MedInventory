4766fc32b27d000f315956aba1505ef86860e768
georgejosephcodes <uhustle1@gmail.com>
2026-01-20 10:38:29 +0530

    changed frontend url

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://med-inventory.vercel.app'
+    'https://medinventory2.netlify.app'
   ],
   credentials: true,
 }));
